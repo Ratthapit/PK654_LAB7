36dbeeac9693745e00d8f083a90c409b5877a0d7
Ratthapit <pimpitchakanitpanyajaroen@gmail.com>
2021-11-30 15:14:57 +0700

    Lab7_2.cpp complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,10 +4,31 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	int i = 0, P[5000], k, result=0;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
+	}
+	
+	cout << "Please input k: ";
+	cin >> k;
+	
+	if(k>0){
+		for(i=0;i<5000;i++){
+			if(P[i]%k!=0){
+				result += P[i];
+			}
+			else{
+				result -= P[i];
+			}
+		}
+		
+		cout <<"Result = "<<result; 
+		
+		return 0;
+	}
+	else{
+		cout << "Invalid input!!!";
 	}
 	
 	return 0;
