0b7f0e1ce9a22abe92c7a472cfde094f479628cb
Ratthapit <pimpitchakanitpanyajaroen@gmail.com>
2021-11-30 19:19:35 +0700

    lab7.3.cpp cp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,7 +1,58 @@
 #include<iostream>
-
 using namespace std;
 
+int adiff(int A,int B){	
+	int a,b;
+	
+	if(A==B){
+    		return 0;
+		}
+		
+	if(A==-B){
+    		return 180;
+		}
+		
+	if(A>=0&&B>=0){
+		a=A%360;
+		b=B%360;
+	}
+	if(A<=0&&B<=0){
+		a=-A%360;
+		b=-B%360;
+	}
+	if(A>0&&B<0){
+		a=A%360;
+		b=(360+B)%360;
+	}
+	
+	if(A<0&&B>0){
+		a=(360+A)%360;
+		b=B%360;
+	}
+	
+	
+	if(a==b){
+    	return 0;
+	}
+		
+	if(a>b){
+    	if(a-b>180){
+    		return 360-(a-b);
+		}
+		else{
+			return a-b;
+		}
+	}
+		
+	if(b>a){
+    	if(b-a>180){
+    		return 360-(b-a);
+		}
+		else{
+			return b-a;
+		}
+	}
+}
 
 int main(){
   cout << adiff(180,270);
