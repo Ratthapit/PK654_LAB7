31fa540c703f121b5b070a76b2d5282c89eaff73
Ratthapit <pimpitchakanitpanyajaroen@gmail.com>
2021-11-30 19:19:41 +0700

    lab7.3.cpp cp

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,36 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+    	int i,j;
+	double x,GPA;
+	string y="";
+	
+    for(i=0;i<10;i++){
+    	for(j=0;j<9;j++){
+    		if(id[j]>id[j+1]){
+    			x=id[j];
+    			id[j]=id[j+1];
+    			id[j+1]=x; 
+    			
+    			x=gpa[j];
+    			gpa[j]=gpa[j+1];
+    			gpa[j+1]=x;
+    			
+    			y=name[j];
+    			name[j]=name[j+1];
+    			name[j+1]=y;
+			}
+		}
+	}
+	
+	cout <<"Please input GPA: " ;
+	cin>>GPA;
+	
+	for(i=0;i<10;i++){
+		if(gpa[i]>=GPA){
+			cout <<id[i]<<"	"<<name[i]<<gpa[i]<<"\n";
+		}
+	}
 		
 	return 0;
 }
